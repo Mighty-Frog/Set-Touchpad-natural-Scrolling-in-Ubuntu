dd6c5ce9a4d0a3d8746d13155cd61da207b8fff3
Jin Cheng <78740875+Mighty-Frog@users.noreply.github.com>
2021-11-14 04:57:27 +0000

    Update touchpadNatrualScrolling.sh

--- touchpadNatrualScrolling.sh
+++ touchpadNatrualScrolling.sh
@@ -1,5 +1,6 @@
-xinput list
+#!/bin/bash
+#input list
 
+# xinput set-button-map "your Touchpad device name on the list " 1 2 3 5 4 7 6 8 9 10 11 12
 
-xinput set-button-map "ELAN1207:00 04F3:310E Touchpad" 1 2 3 5 4 7 6 8 9 10 11 12
-
+exit 0
